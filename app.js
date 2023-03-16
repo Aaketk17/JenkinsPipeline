a0ed9d181d66b4a579d4ca2b6fad6396e203e1d2
Aaketk17 <thavanthya@gmail.com>
2023-03-16 23:29:18 +0530

    Created a basic nodejs RestAPI

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+const server = require('http').createServer(app)
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(morgan('common'))
+app.use(express.json())
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('Helllo this is a sample route')
+})
+
+server.listen(8000, () => {
+  console.log(`Server is listing at port Number 8000`)
+})
